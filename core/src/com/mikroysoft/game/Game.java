7f7ec75f52ba387119b98a55359e3022b5513521
AngrySaltyFish <james.rand1@aol.co.uk>
2019-11-14 16:44:05 +0000

    Working initial build with logo

--- core/src/com/mikroysoft/game/Game.java
+++ core/src/com/mikroysoft/game/Game.java
@@ -13,15 +13,15 @@ public class Game extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("logo.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 0, 0, 100, 100);
 		batch.end();
 	}
 	
