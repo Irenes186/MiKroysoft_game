fce1965278107fc5c6174d8a59816885f96be700
AngrySaltyFish <james.rand1@aol.co.uk>
2019-11-21 19:31:24 +0000

    Adding the missing Road class

---
+++ core/src/com/mikroysoft/game/Road.java
@@ -0,0 +1,26 @@
+package com.mikroysoft.game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Road implements IRenderable {
+
+    private Texture texture;
+    public Coordinate position;
+    private int TILEWIDTH, TILEHEIGHT;
+
+    public Road (Coordinate position, int TILEWIDTH, int TILEHEIGHT) {
+        this.position = position;
+        this.texture = new Texture("road.png");
+        this.TILEWIDTH = TILEWIDTH;
+        this.TILEHEIGHT = TILEHEIGHT;
+
+
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+        batch.draw(texture, position.x, position.y, TILEWIDTH, TILEHEIGHT);
+    }
+
+}
