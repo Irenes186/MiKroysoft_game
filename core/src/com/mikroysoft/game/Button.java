d99b5ac7646abb8bcf885a3bd11031080b615a73
cec566 <56448589+cec566@users.noreply.github.com>
2019-11-21 14:26:57 +0000

    Create Button.java
    class implementing a button

---
+++ core/src/com/mikroysoft/game/Button.java
@@ -0,0 +1,30 @@
+package com.mikroysoft.game;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class Button {
+	
+	public Texture texture;
+	public Coordinate position;
+	public int height;
+	public int width;
+	public boolean hover;
+	
+	public Button(int height, int width, String filename, Coordinate position) {
+		this.height = height;
+		this.width = width;
+		this.texture = new Texture(filename);
+		this.position = position;
+		hover = false;
+		
+		
+	}
+	
+	public void hover(Coordinate input){
+		hover = (input.x <= position.x + width) && (input.x >= position.x) 
+				&& (input.y <= position.y + height) && (input.y >= position.y);
+	}
+	
+	
+
+}
