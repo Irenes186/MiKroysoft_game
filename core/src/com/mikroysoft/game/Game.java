6aa8f9b53c7b6386f12b100ba8e83d099adabaca
aj1134 <44804358+aj1134@users.noreply.github.com>
2019-11-29 15:35:27 +0000

    Fuel
    Added fuel stuff.

--- core/src/com/mikroysoft/game/Game.java
+++ core/src/com/mikroysoft/game/Game.java
@@ -14,6 +14,8 @@ public class Game extends ApplicationAdapter {
     Map map;
     ProgressBar health;
     Texture healthIcon;
+    ProgressBar fuel;
+    Texture fuelIcon;
 	Alien[] aliens;
 	
 	@Override
@@ -40,15 +42,25 @@ public class Game extends ApplicationAdapter {
 		aliens[0] = new Alien( new Coordinate(100, 100), 2, 2);
 		
 		//health progress bar:
-		health = new ProgressBar();
+		health = new ProgressBar(1);
 		health.setPosition(20,10);
 		health.setDimensions(100,10);
 		health.setMax(100);
 		health.updateCurrent(100);
-
 		
 		//health icon - next to health progress bar.
 		healthIcon = new Texture("health.png");
+		
+		
+		//fuel progress bar:
+		fuel = new ProgressBar(2);
+		fuel.setPosition(20, 25);
+		fuel.setDimensions(100,10);
+		fuel.setMax(100);
+		fuel.updateCurrent(100);
+		
+		//fuel icon - next to fuel progress bar.
+		fuelIcon = new Texture("fuel.png");
 	}
 
 	@Override
@@ -65,8 +77,12 @@ public class Game extends ApplicationAdapter {
 		}
 
 		aliens[0].Run();
+		//health
 		batch.draw(health.texture,health.position.x,health.position.y, health.getFill(), health.getHeight());
 		batch.draw(healthIcon,health.position.x - (5 + health.getHeight()), health.position.y, health.getHeight(), health.getHeight());
+		//fuel
+		batch.draw(fuel.texture,fuel.position.x,fuel.position.y, fuel.getFill(), fuel.getHeight());
+		batch.draw(fuelIcon,fuel.position.x - (5 + fuel.getHeight()), fuel.position.y, fuel.getHeight(), fuel.getHeight());
 		for (Alien alien: aliens) {
 			batch.draw(alien.texture,alien.position.x,Gdx.graphics.getHeight()-alien.position.y,40,40,40,40,1,1,alien.direction,0,0,16,16,false,false);
 		}
