9c8088333c6f81da2b0325cffa8c67d4fa084efc
AngrySaltyFish <james.rand1@aol.co.uk>
2020-01-17 17:37:10 +0000

    Sorting fireengine speed

--- core/src/com/mikroysoft/game/FireEngineParameters.java
+++ core/src/com/mikroysoft/game/FireEngineParameters.java
@@ -10,20 +10,20 @@ public class FireEngineParameters{
     public FireEngineParameters(int baseIndex) {
         switch(baseIndex) {
             case 0:
-                acceleration = 0.10f;
-                maxSpeed = 1.00f;
-                break;
-            case 1:
                 acceleration = 0.50f;
                 maxSpeed = 2.00f;
                 break;
-            case 2:
-                acceleration = 0.01f;
-                maxSpeed = 0.05f;
-                break;
-            default:
-                acceleration = 0.10f;
+            case 1:
+                acceleration = 1.00f;
                 maxSpeed = 2.00f;
+                break;
+            case 2:
+                acceleration = 0.20f;
+                maxSpeed = 1.00f;
+                break;
+            case 3:
+                acceleration = 0.50f;
+                maxSpeed = 1.00f;
                 break;
         }
     }
