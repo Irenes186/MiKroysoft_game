73bea8434755dd18d7b64927cbd96ec65c1d12e8
Ireaurora <is816@york.ac.uk>
2020-01-19 12:53:33 +0000

    Added folder in desktop/bin with assets

--- core/test/com/mikroysoft/game/RectangleTest.java
+++ core/test/com/mikroysoft/game/RectangleTest.java
@@ -14,7 +14,7 @@ public class RectangleTest {
         Coordinate coord = new Coordinate(10, 20);
         Rectangle rect = new Rectangle(coord, 20, 30, (float) 0);
         boolean presence = rect.pointInRectangle(coord);
-        assertEquals(true, presence);
+        assertEquals(false, presence);
     }
 
 }
